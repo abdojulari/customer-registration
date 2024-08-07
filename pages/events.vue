b67b984cea1fbe071cc011a08266605185675deb
Abdul Ojulari <abdulkabirojulari@gmail.com>
2024-08-07 11:54:01 -0600

    nuxt with tailwind configuration

---
+++ pages/events.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1> I am an Event</h1>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <section class="w-full p-10">
+        <h1 class="leading-10 text-4xl text-blue-700">Home</h1>
+        <p>Welcome to the home page</p>
+        
+    </section>
+</template>
